09cddb3e06c1febd5c3515d097fd4e099fda8e8b
vtog <v.tognaci@f5.com>
2019-04-17 20:23:35 -0500

    terraform kube deployment and supporting ansible workbooks

--- main.tf
+++ main.tf
@@ -2,22 +2,41 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "kube-master1" {
+  ami             = "ami-0a313d6098716f372" # Ubuntu 18.04 LTS AMD64
+  instance_type   = "t2.medium"
+  key_name        = "${aws_key_pair.corp-deb-key.key_name}"
+  security_groups = ["${aws_security_group.kube_sg.name}","${aws_security_group.allow_ssh.name}"]
+
+  tags = {
+    Name = "kube-master1"
+  }
+}
+
+resource "aws_instance" "kube-node1" {
   ami             = "ami-0a313d6098716f372" # Ubuntu 18.04 LTS AMD64
   instance_type   = "t2.micro"
-  key_name        = "${aws_key_pair.my-key.key_name}"
-  security_groups = ["${aws_security_group.allow_ssh.name}"]
+  key_name        = "${aws_key_pair.corp-deb-key.key_name}"
+  security_groups = ["${aws_security_group.kube_sg.name}","${aws_security_group.allow_ssh.name}"]
+
+  tags = {
+    Name = "kube-node1"
+  }
 }
 
-resource "aws_instance" "kube-master1" {
-    ami             = "ami-0a313d6098716f372" # Ubuntu 18.04 LTS AMD64
-    instance_type   = "t2.micro"
-    key_name        = "${aws_key_pair.my-key.key_name}"
-    security_groups = ["${aws_security_group.allow_ssh.name}"]
+resource "aws_instance" "kube-node2" {
+  ami             = "ami-0a313d6098716f372" # Ubuntu 18.04 LTS AMD64
+  instance_type   = "t2.micro"
+  key_name        = "${aws_key_pair.corp-deb-key.key_name}"
+  security_groups = ["${aws_security_group.kube_sg.name}","${aws_security_group.allow_ssh.name}"]
+
+  tags = {
+    Name = "kube-node2"
+  }
 }
 
-resource "aws_key_pair" "my-key" {
-  key_name   = "my-key"
+resource "aws_key_pair" "corp-deb-key" {
+  key_name   = "corp-deb-key"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
@@ -28,15 +47,35 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["99.36.4.190/32"]
+  }
+}
+
+resource "aws_security_group" "kube_sg" {
+  name = "kube_sg"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["172.31.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-output "example_public_dns" {
-  value = "${aws_instance.example.public_dns}"
-}
 
 output "kube-master1__public_dns" {
-    value = "${aws_instance.kube-master1.public_dns}"
+  value = "${aws_instance.kube-master1.public_dns}"
+}
+output "kube-node1__public_dns" {
+  value = "${aws_instance.kube-node1.public_dns}"
+}
+output "kube-node2__public_dns" {
+  value = "${aws_instance.kube-node2.public_dns}"
 }
 
