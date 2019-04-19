3a16b226924d04e7d57c2339d9b5be44bce0a6e2
vtog <v.tognaci@f5.com>
2019-04-19 07:41:38 -0500

    added dynamic inventory file for ansible

--- main.tf
+++ main.tf
@@ -1,13 +1,17 @@
 provider "aws" {
-  region     = "us-east-1"
+  region = "${var.aws_region}"
+}
+
+# Get My Public IP
+data "http" "myIP" {
+  url = "http://ipv4.icanhazip.com"
 }
 
 resource "aws_instance" "kube-master1" {
   ami             = "ami-0a313d6098716f372" # Ubuntu 18.04 LTS AMD64
-  instance_type   = "t2.medium"
+  instance_type   = "t2.micro"
   key_name        = "${aws_key_pair.corp-deb-key.key_name}"
-  security_groups = ["${aws_security_group.kube_sg.name}","${aws_security_group.allow_ssh.name}"]
-
+  security_groups = ["${aws_security_group.kube_sg.name}"]
   tags = {
     Name = "kube-master1"
   }
@@ -17,8 +21,7 @@ resource "aws_instance" "kube-node1" {
   ami             = "ami-0a313d6098716f372" # Ubuntu 18.04 LTS AMD64
   instance_type   = "t2.micro"
   key_name        = "${aws_key_pair.corp-deb-key.key_name}"
-  security_groups = ["${aws_security_group.kube_sg.name}","${aws_security_group.allow_ssh.name}"]
-
+  security_groups = ["${aws_security_group.kube_sg.name}"]
   tags = {
     Name = "kube-node1"
   }
@@ -28,8 +31,7 @@ resource "aws_instance" "kube-node2" {
   ami             = "ami-0a313d6098716f372" # Ubuntu 18.04 LTS AMD64
   instance_type   = "t2.micro"
   key_name        = "${aws_key_pair.corp-deb-key.key_name}"
-  security_groups = ["${aws_security_group.kube_sg.name}","${aws_security_group.allow_ssh.name}"]
-
+  security_groups = ["${aws_security_group.kube_sg.name}"]
   tags = {
     Name = "kube-node2"
   }
@@ -40,42 +42,66 @@ resource "aws_key_pair" "corp-deb-key" {
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name = "allow_ssh"
-  # SSH access
+resource "aws_security_group" "kube_sg" {
+  name = "kube_sg"
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["99.36.4.190/32"]
+    cidr_blocks = ["${chomp(data.http.myIP.body)}/32"]
   }
-}
-
-resource "aws_security_group" "kube_sg" {
-  name = "kube_sg"
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["172.31.0.0/16"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = "kube_sg"
+  }
 }
 
+# write out host file
+data "template_file" "inventory" {
+template = <<EOF
+[kube-all]
+${aws_instance.kube-master1.tags.Name} ansible_host=${aws_instance.kube-master1.public_ip} private_ip=${aws_instance.kube-master1.private_ip}
+${aws_instance.kube-node1.tags.Name} ansible_host=${aws_instance.kube-node1.public_ip} private_ip=${aws_instance.kube-node1.private_ip}
+${aws_instance.kube-node2.tags.Name} ansible_host=${aws_instance.kube-node2.public_ip} private_ip=${aws_instance.kube-node2.private_ip}
+
+[kube-masters]
+${aws_instance.kube-master1.tags.Name} ansible_host=${aws_instance.kube-master1.public_ip}
+
+[kube-nodes]
+${aws_instance.kube-node1.tags.Name} ansible_host=${aws_instance.kube-node1.public_ip}
+${aws_instance.kube-node2.tags.Name} ansible_host=${aws_instance.kube-node2.public_ip}
+
+[all:vars]
+ansible_user=ubuntu
+ansible_become=true
+ansible_python_interpreter=/usr/bin/python3
+EOF
+}
+
+resource "local_file" "save_inventory" {
+  depends_on = ["data.template_file.inventory"]
+  content = "${data.template_file.inventory.rendered}"
+  filename = "./kubernetes/ansible/inventory.ini"
+}
 
 output "kube-master1__public_dns" {
-  value = "${aws_instance.kube-master1.public_dns}"
+    value = "${aws_instance.kube-master1.public_dns}"
 }
 output "kube-node1__public_dns" {
-  value = "${aws_instance.kube-node1.public_dns}"
+    value = "${aws_instance.kube-node1.public_dns}"
 }
 output "kube-node2__public_dns" {
-  value = "${aws_instance.kube-node2.public_dns}"
+    value = "${aws_instance.kube-node2.public_dns}"
 }
 
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "vpc_cidr" {
+  default = "172.31.0.0/16"
+}
+variable "ssh_key" {
+  default = "udf-key"
+}
